b7a17a55c151d1a256de15fc16b2786f99edd38e
Ivan Alexandrov <radniforg@yandex.ru>
2020-09-24 15:30:14 +0300

    basic resources obtained

--- Phones/Market/models.py
+++ Phones/Market/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+class Shop(models.Model):
+    name = models.CharField(max_length=40)
+    state = models.BooleanField(default=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+
+class Product(models.Model):
+    name = models.CharField(max_length=40)
