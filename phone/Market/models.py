3f15745e608c1d0ae5b3ea31593f0012e8fc79f7
Ivan Alexandrov <radniforg@yandex.ru>
2020-10-06 17:28:30 +0300

    initial migration

--- phone/Market/models.py
+++ phone/Market/models.py
@@ -1,10 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import date
 
 # Create your models here.
-class User(AbstractUser):
-    pass
+# class User(AbstractUser):
+#     pass
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=64)
+    picture_link = models.CharField(max_length=120)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, blank=True, null=True)
+    information = models.TextField()
+    date = models.DateField(default=date.today())
+
+    def __str__(self):
+        return self.name
